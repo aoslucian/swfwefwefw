3ae05513b16f4439ce65481c8ca46442d637e296
Lucian Rosu <lucian.rosu@adopstar.com>
2024-03-27 14:50:49 +0000

    Add ParticlesContainer component and update layout***
    ***Add new dependencies and update tailwind configuration

---
+++ components/Hero.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import ParticlesContainer from './ParticlesContainer'
+
+export default function Hero() {
+  return (
+    <div className='flex justify-center items-center '>
+
+<ParticlesContainer />
+
+       </div>
+  )
+}
